09c46e8e9612e42589f3d4a1b7d6a44ae1691c6c
Maciej Głowiński <maciej.glowinski@comarch.pl>
2019-06-05 16:43:58 +0200

    add neuron class

---
+++ src/Neuron.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class Neuron {
+
+    private double[] inputs;
+    private double[] weights;
+
+    public Neuron(int numberOfWeights) {
+        Random random = new Random();
+        weights = new double[numberOfWeights];
+        for (int i = 0; i < numberOfWeights; i++) {
+            weights[i] = random.nextDouble();
+        }
+    }
+
+    public void setInputs(double[] inputs) {
+        this.inputs = inputs;
+    }
+
+    public void setWeights(double[] weights) {
+        this.weights = weights;
+    }
+
+    public double[] getWeights() {
+        return weights;
+    }
+
+    public double countOutput() {
+        double sum = 0;
+        for (int i = 0; i < inputs.length; i++) {
+            sum += Math.pow(inputs[i] - weights[i], 2);
+        }
+        return Math.sqrt(sum);
+    }
+
+}
